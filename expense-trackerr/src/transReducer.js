b9a326b2742e498f27ed712fb9adf7c6f55a9730
ahmedhassan2k <ahmedhassan2k@gmail.com>
2021-07-02 03:04:43 +0500

    Added case for delete transaction

--- expense-trackerr/src/transReducer.js
+++ expense-trackerr/src/transReducer.js
@@ -3,6 +3,9 @@ export const transReducer = ((state, action)=>{
         case 'ADD':{
             return [action.payload, ...state]
         }
+        case 'DELETE':{
+            return [ ...state.filter(transaction => transaction.id !== action.payload)]
+        }
         default:{
             return state;
         }
